992546fad871b0ede474a0e6782fbb427e7baddd
Daniel Doblado <danieldoblado@gmail.com>
2019-09-16 11:29:33 +0200

    Read schemaURL from url parameters

--- src/components/SkohubEditor.js
+++ src/components/SkohubEditor.js
@@ -15,27 +15,47 @@ class SkohubEditor extends React.Component {
 
   constructor(props) {
     super(props)
+    const url = new URL(window.location.href)
 
     this.state = {
       json: {},
-      schema: null
+      schema: null,
+      schemaURL: url.searchParams.get('schema')
     }
+    this.setSchema = this.setSchema.bind(this)
+    this.setSchemaURL = this.setSchemaURL.bind(this)
+  }
+
+  componentDidMount() {
+    const { setSchema, state: { schemaURL } } = this
+    schemaURL && setSchema(schemaURL)
+  }
+
+  setSchema(schemaURL) {
+    fetch(schemaURL).then(response => {
+      response.json().then(schema => {
+        this.setState({schema})
+      })
+    }).catch(err => console.log(err))
+  }
+
+  setSchemaURL(url) {
+    this.setState({schemaURL: url})
   }
 
   render() {
-    const {json, schema} = this.state
+    const {
+      setSchema,
+      setSchemaURL,
+      state: { json, schema, schemaURL }
+    } = this
 
     return (
       <div className="wrapper">
         <Header
-          setSchema={(schemaURL) => {
-            console.log(schemaURL)
-            fetch(schemaURL).then(response => {
-              response.json().then(schema => {
-                this.setState({schema})
-              })
-            }).catch(err => console.log(err))
-          }}
+          setSchema={setSchema}
+          schemaURL={schemaURL}
+          setSchemaURL={setSchemaURL}
         />
         <main className="content">
             {schema && (
