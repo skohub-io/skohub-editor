e0422c328a9d7268fa238c76cfff074d370e723b
Pascal Christoph <christoph@hbz-nrw.de>
2020-05-11 10:38:19 +0200

    Complete LSB comment

--- scripts/etc/init.d/skohub-editor.sh
+++ scripts/etc/init.d/skohub-editor.sh
@@ -3,6 +3,8 @@
 ### BEGIN INIT INFO
 # Provides:          skohub-editor
 # Should-Start:      $monit
+# Required-Start:
+# Required-Stop:
 # Default-Start:     2 3 4 5
 # Default-Stop:      0 1 6
 # Short-Description: skohub-editor as a node server
