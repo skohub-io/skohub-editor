e2bb94b5083770c5da1a0899a5de221ea0fba8f3
@s.roertgen <sroertgen@gmail.com>
2021-04-20 16:54:08 +0200

    add possibility to fetch vocabs hostes with GH-Pages
    This is just a quick test to be able to pull vocabs from GitHub Pages.
    
    Since GH-Pages does not do any Content Negotiation, I quickly (and dirty) implemented a way to pull those value lists.
    Basically it checks the url and appends an /index.json or /index.index to the url.
    
    If that fails it just uses the normal `schemaLocation`-url.
    
    this can definitley be improved.

--- src/components/JSONPointerForm/SkohubLookup.js
+++ src/components/JSONPointerForm/SkohubLookup.js
@@ -1,43 +1,56 @@
 /* global fetch, alert */
 
-import React, { useEffect, useState } from 'react'
-import PropTypes from 'prop-types'
-import FlexSearch from 'flexsearch'
+import React, { useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import FlexSearch from "flexsearch";
 
-import withFormData from './withFormData'
+import withFormData from "./withFormData";
 
-const getNestedItems = item => {
-  let ids = [item.id]
+const getNestedItems = (item) => {
+  let ids = [item.id];
   if (item.narrower) {
-    item.narrower.forEach(narrower => {
-      ids = ids.concat(getNestedItems(narrower))
-    })
+    item.narrower.forEach((narrower) => {
+      ids = ids.concat(getNestedItems(narrower));
+    });
   }
-  return ids
-}
+  return ids;
+};
 
-const NestedList = ({ items, filter, highlight, setValue, setQuery, setExpanded, translate }) => {
+const NestedList = ({
+  items,
+  filter,
+  highlight,
+  setValue,
+  setQuery,
+  setExpanded,
+  translate,
+}) => {
   const filteredItems = filter
-    ? items.filter(item => !filter || filter.some(filter => getNestedItems(item).includes(filter)))
-    : items
+    ? items.filter(
+        (item) =>
+          !filter ||
+          filter.some((filter) => getNestedItems(item).includes(filter))
+      )
+    : items;
 
   return (
     <ul>
-      {filteredItems.map(item => (
-        <li
-          key={item.id}
-        >
+      {filteredItems.map((item) => (
+        <li key={item.id}>
           <span
             dangerouslySetInnerHTML={{
-              __html: translate(item.prefLabel).replace(highlight, str => `<strong>${str}</strong>`)
+              __html: translate(item.prefLabel).replace(
+                highlight,
+                (str) => `<strong>${str}</strong>`
+              ),
             }}
             onClick={() => {
-              setValue(item)
-              setQuery(null)
-              setExpanded(false)
+              setValue(item);
+              setQuery(null);
+              setExpanded(false);
             }}
           />
-          {item.narrower &&
+          {item.narrower && (
             <NestedList
               items={item.narrower}
               filter={filter}
@@ -47,121 +60,184 @@ const NestedList = ({ items, filter, highlight, setValue, setQuery, setExpanded,
               setExpanded={setExpanded}
               translate={translate}
             />
-          }
+          )}
         </li>
       ))}
     </ul>
-  )
-}
+  );
+};
 
 const SkohubLookup = (props) => {
-  const [index, setIndex] = useState(FlexSearch.create('speed'))
-  const [query, setQuery] = useState(null)
-  const [scheme, setScheme] = useState(null)
-  const [expanded, setExpanded] = useState(false)
+  const [index, setIndex] = useState(FlexSearch.create("speed"));
+  const [query, setQuery] = useState(null);
+  const [scheme, setScheme] = useState(null);
+  const [expanded, setExpanded] = useState(false);
 
   const {
-    schema, value, setValue, title, property, errors, name, required, formId, translate,
-    registerSubmitCallback, options
-  } = props
-  const schemaLocation = (options.url || schema.properties.inScheme.properties.id.enum[0]).replace(/^https?:/, '')
+    schema,
+    value,
+    setValue,
+    title,
+    property,
+    errors,
+    name,
+    required,
+    formId,
+    translate,
+    registerSubmitCallback,
+    options,
+  } = props;
+  const schemaLocation = (
+    options.url || schema.properties.inScheme.properties.id.enum[0]
+  ).replace(/^https?:/, "");
 
-  value && registerSubmitCallback(data => {
-    fetch(value.id.replace(/^http:/, '')).then(response => {
-      const actor = new URL(response.url).pathname.substring(1)
-      const label = translate(value.prefLabel)
-      fetch(`/inbox?actor=${actor}`, {
-        method: 'post',
-        mode: 'cors',
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/ld+json'
-        },
-        body: JSON.stringify(data)
-      }).then(res => alert(res.ok ? `Published to ${label}` : `Could not publish to ${label}`))
-        .catch(e => alert(`An error has occured: ${e.message}`))
-    })
-  })
+  value &&
+    registerSubmitCallback((data) => {
+      fetch(value.id.replace(/^http:/, "")).then((response) => {
+        const actor = new URL(response.url).pathname.substring(1);
+        const label = translate(value.prefLabel);
+        fetch(`/inbox?actor=${actor}`, {
+          method: "post",
+          mode: "cors",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/ld+json",
+          },
+          body: JSON.stringify(data),
+        })
+          .then((res) =>
+            alert(
+              res.ok ? `Published to ${label}` : `Could not publish to ${label}`
+            )
+          )
+          .catch((e) => alert(`An error has occured: ${e.message}`));
+      });
+    });
+
+  // Prettify Me this is ugly, but it works
+  const schemaFetch = (error = false) => {
+    const headers = {
+      Accept: "application/json",
+    };
+    if (error) {
+      return fetch(schemaLocation, { headers: headers });
+    } else {
+      return fetch(
+        schemaLocation.endsWith("index.json")
+          ? schemaLocation
+          : schemaLocation.endsWith("/")
+          ? schemaLocation.concat("index.json")
+          : schemaLocation.concat("/index.json"),
+        { headers: headers }
+      );
+    }
+  };
 
   useEffect(() => {
-    fetch(schemaLocation, {
-      headers: {
-        Accept: 'application/json'
-      }
-    }).then(response => response.json())
-      .then(setScheme)
-
-    fetch(schemaLocation, {
-      headers: {
-        Accept: 'text/index'
-      }
-    }).then(response => response.json())
-      .then(serialized => {
-        const idx = FlexSearch.create()
-        idx.import(serialized)
-        setIndex(idx)
-        console.log('index loaded', idx.info())
+    schemaFetch()
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Something went wrong");
+        }
+        return response.json();
       })
-  }, [])
+      .then(setScheme)
+      .catch((error) => {
+        console.error(
+          "There has been a problem with your fetch operation:",
+          error
+        );
+        schemaFetch(error=true)
+          .then((response) => response.json())
+          .then(setScheme);
+      });
+
+    fetch(
+      schemaLocation.endsWith("index.json")
+        ? schemaLocation.replace(/.json?/, ".index")
+        : schemaLocation.endsWith("/")
+        ? schemaLocation.concat("index.index")
+        : schemaLocation,
+      {
+        headers: {
+          Accept: "text/index",
+        },
+      }
+    )
+      .then((response) => response.json())
+      .then((serialized) => {
+        const idx = FlexSearch.create();
+        idx.import(serialized);
+        setIndex(idx);
+        console.log("index loaded", idx.info());
+      });
+  }, []);
 
   return (
     <div
-      className={`SkohubLookup ${property || ''} ${errors.length ? 'hasError' : ''}`.trim()}
+      className={`SkohubLookup ${property || ""} ${
+        errors.length ? "hasError" : ""
+      }`.trim()}
       role="group"
       aria-labelledby={`${formId}-${name}-label`}
     >
       <div
-        className={`label ${required ? 'required' : ''}`.trim()}
+        className={`label ${required ? "required" : ""}`.trim()}
         id={`${formId}-${name}-label`}
       >
         {title}
       </div>
-      {scheme && (
-        value ? (
-          <div className="skohubLookupSelectedValue" onClick={() => setValue(undefined)}>
+      {scheme &&
+        (value ? (
+          <div
+            className="skohubLookupSelectedValue"
+            onClick={() => setValue(undefined)}
+          >
             {translate(value.prefLabel)}
           </div>
         ) : (
           <div className="skohubLookupContent">
             <input
-              value={query || ''}
+              value={query || ""}
               type="text"
-              onChange={e => {
-                setQuery(e.target.value || null)
-                setExpanded(true)
+              onChange={(e) => {
+                setQuery(e.target.value || null);
+                setExpanded(true);
               }}
               onFocus={() => setExpanded(true)}
-              onBlur={() => setTimeout(() => {
-                setExpanded(false)
-                setQuery(null)
-              }, 250
-              )}
+              onBlur={() =>
+                setTimeout(() => {
+                  setExpanded(false);
+                  setQuery(null);
+                }, 250)
+              }
               placeholder={title}
             />
-            {expanded &&
-                <NestedList
-                  items={scheme.hasTopConcept}
-                  filter={query ? index.search(query) : null}
-                  highlight={RegExp(query, 'gi')}
-                  setValue={item => setValue({
+            {expanded && (
+              <NestedList
+                items={scheme.hasTopConcept}
+                filter={query ? index.search(query) : null}
+                highlight={RegExp(query, "gi")}
+                setValue={(item) =>
+                  setValue({
                     id: item.id,
                     prefLabel: item.prefLabel,
-                    type: 'Concept',
+                    type: "Concept",
                     inScheme: {
-                      id: schema.properties.inScheme.properties.id.enum[0]
-                    }
-                  })}
-                  setQuery={setQuery}
-                  setExpanded={setExpanded}
-                  translate={translate}
-                />
-            }
+                      id: schema.properties.inScheme.properties.id.enum[0],
+                    },
+                  })
+                }
+                setQuery={setQuery}
+                setExpanded={setExpanded}
+                translate={translate}
+              />
+            )}
           </div>
-        )
-      )}
+        ))}
     </div>
-  )
-}
+  );
+};
 
 NestedList.propTypes = {
   translate: PropTypes.func.isRequired,
@@ -170,8 +246,8 @@ NestedList.propTypes = {
   setExpanded: PropTypes.func,
   items: PropTypes.arrayOf(PropTypes.any),
   filter: PropTypes.arrayOf(PropTypes.any),
-  highlight: PropTypes.objectOf(PropTypes.instanceOf(RegExp))
-}
+  highlight: PropTypes.objectOf(PropTypes.instanceOf(RegExp)),
+};
 
 SkohubLookup.propTypes = {
   translate: PropTypes.func.isRequired,
@@ -185,7 +261,7 @@ SkohubLookup.propTypes = {
   setValue: PropTypes.func,
   registerSubmitCallback: PropTypes.func,
   required: PropTypes.bool,
-  options: PropTypes.objectOf(PropTypes.any)
-}
+  options: PropTypes.objectOf(PropTypes.any),
+};
 
-export default withFormData(SkohubLookup)
+export default withFormData(SkohubLookup);
